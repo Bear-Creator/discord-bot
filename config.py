23fafd79d3d5ff21fa147661685341ca31242156
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2022-02-15 09:01:14 +0300

    Added a hello-bot

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ settings = {
     'token': 'OTQyNDkxODE3NjI5MzIzMzE1.YglR5w.LgGf3jPVUEairyoHvItchwi2hFo',
     'bot': 'Таможня',
     'id':  942491817629323315,
-    'prefix': '/'
+    'prefix': '$'
 }
