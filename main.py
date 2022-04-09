1aa699319045b4cb354baceda4338c0f29ef2ef0
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2022-04-09 11:32:30 +0300

    /

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import discord
 from discord.ext import commands
-from config import settings
+from config import settings 
 
-bot = commands.Bot(command_prefix = settings['prefix'])
+bot = commands.Bot(command_prefix='!')
 
-@bot.command() # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
-async def hello(ctx): # Создаём функцию и передаём аргумент ctx.
-    author = ctx.message.author # Объявляем переменную author и записываем туда информацию об авторе.
-    await ctx.send(f'Hello, {author.mention}!') # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
+@bot.event
+async def on_ready():
+    print("Everything's all ready to go~")
+    
 
-bot.run(settings['token']) # Обращаемся к словарю settings с ключом token, для получения токена
+@bot.event
+async def on_message(msg):
+    author = msg.author
+    if author != bot.user:
+        text = msg.content
+        print(f'{author}: {text}')
+        await msg.reply('Я пока не работаю')
+
+
+
+bot.run(settings['token'])
---
+++ users.sql
@@ -0,0 +1,5 @@
+CREATE TABLE weather (
+    city            varchar(80),
+    temp_lo         int,           -- low temperature
+    temp_hi         int,           -- high temperature
+);
