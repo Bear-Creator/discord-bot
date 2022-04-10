731975d5c582dc703b800bc0befec406b12bb856
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2022-04-10 15:59:26 +0300

    SQL

--- users.sql
+++ users.sql
@@ -1,5 +1,8 @@
-CREATE TABLE weather (
-    city            varchar(80),
-    temp_lo         int,           -- low temperature
-    temp_hi         int,           -- high temperature
+CREATE TABLE users (
+    id          int,
+    username    varchar(80),
+    firstname   varchar(80),
+    middlename  varchar(80),
+    surname     varchar(80),
+    birthday    date
 );
