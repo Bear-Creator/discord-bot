e383ff0c6c27357cfa1b92c05bacdb016dcaeac2
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2022-04-10 15:59:41 +0300

    SQL

--- config.py
+++ config.py
@@ -2,5 +2,12 @@ settings = {
     'token': 'OTQyNDkxODE3NjI5MzIzMzE1.YglR5w.LgGf3jPVUEairyoHvItchwi2hFo',
     'bot': 'Таможня',
     'id':  942491817629323315,
-    'prefix': '$'
+    'prefix': '$',
+}
+db = {
+    'user':'postgres',
+    'password': '1',
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'discord'
 }
--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 from discord.ext import commands
-from config import settings 
+import psycopg2
+from psycopg2 import Error
+from config import settings, db
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix=settings['prefix'])
+
+try:
+    # Подключение к существующей базе данных
+    db = psycopg2.connect(user=db['user'],
+                          password=db['password'],
+                          host=db['host'],
+                          port=db['port'],
+                          database=db['database'])
+
+    #Курсор для выполнения операций с базой данных
+    curs = db.cursor()
+
+    # Распечатать сведения о PostgreSQL
+    print("Информация о сервере PostgreSQL")
+    print(db.get_dsn_parameters(), "\n")
+
+    # Выполнение SQL-запроса
+    curs.execute("SELECT version();")
+
+    # Получить результат
+    print("Вы подключены к - ", curs.fetchone(), "\n")
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+
 
 @bot.event
 async def on_ready():
-    print("Everything's all ready to go~")
+    print("Бот включен\n ")
     
+    global db, curs
+    
+
 
 @bot.event
 async def on_message(msg):
@@ -14,7 +44,7 @@ async def on_message(msg):
     if author != bot.user:
         text = msg.content
         print(f'{author}: {text}')
-        await msg.reply('Я пока не работаю')
+        
 
 
 
