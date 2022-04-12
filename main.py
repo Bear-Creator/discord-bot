8ab1b0f57b5c2d0e35f254e31bc35f9326006c61
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2022-04-12 12:53:41 +0300

    Merge branch 'SQLite3' of https://github.com/Bear-Creator/discord-bot into SQLite3

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import sqlite3
 from config import settings, db
 
 
+
+def registration():
+    da = 1
+
 bot = commands.Bot(command_prefix=settings['prefix'], intents = discord.Intents.all())
 print('Подключение к БД...')
 db = sqlite3.connect('server.db')
@@ -27,8 +31,8 @@ async def on_ready():
             if curs.execute(f'SELECT id FROM users WHERE id = {member.id}').fetchone() is None and member != bot.user:
                 curs.execute(f'INSERT INTO users ("id", "username") VALUES ("{member.id}", "{member}")')
                 db.commit()
-                await bot.get_channel(settings['regchan']).send(f'@{member}, пройдите регистрацию чтобы начать RP. Если готовы, напишите "Регистрация".')
-                print(member)
+                await bot.get_channel(settings['regchan']).send(f'<@{member.id}>, пройдите регистрацию чтобы начать RP. Если готовы, напишите "$reg".')
+                print(f'{member} добавлен в базу')
             else:
                 pass
     print('Всё готово к работе')
@@ -38,8 +42,8 @@ async def on_member_join(member):
     if curs.execute(f'SELECT id FROM users WHERE id = {member.id}').fetchone() is None:
         curs.execute(f'INSERT INTO users ("id", "username") VALUES ("{member.id}", "{member}")')
         db.commit()
-        await bot.get_channel(settings['regchan']).send(f'@{member}, пройдите регистрацию чтобы начать RP. Если готовы, напишите "$reg".')
-        print(member)
+        await bot.get_channel(settings['regchan']).send(f'<@{member.id}>, пройдите регистрацию чтобы начать RP. Если готовы, напишите "$reg".')
+        print(f'{member} добавлен в базу')
     else:
         pass
 
@@ -54,7 +58,18 @@ async def reg(ctx):
     }
     channel = await guild.create_text_channel(f'регистрация {member}', overwrites = overwrites)
     await ctx.reply(f'Переходи в эту комнату: <#{channel.id}>')
-    await bot.get_channel(channel.id).send('Приступим! Ты готов(-а)?')
+    await bot.get_channel(channel.id).send('Приступим!')
+    await bot.get_channel(channel.id).send('Придумай легенду своего пресонажа')
+    await bot.get_channel(channel.id).send('Напишите имя персонажа')
+    
+
+@bot.command() #Delete user info
+async def deluser(cxt, a: int):
+    user = curs.execute(f'SELECT username FROM users WHERE id = {a};').fetchone()
+    curs.execute(f'DELETE FROM users WHERE id = {a};')
+    db.commit() 
+    print(f'Пользователь {user} удалён из базы')
+    await cxt.reply(f'Пользователь {user} удалён из базы')
 
 
 @bot.event
@@ -64,12 +79,27 @@ async def on_message(msg):
     if author != bot.user:
         text = msg.content
         print(f'{author}: {text}')
-#        if 'регистрация' in channel: Сегодня доделаю!
+        if 'регистрация' in channel:
+            info = ('firstname', 'middlename', 'surname', 'birthday')
+            data = curs.execute(f'SELECT firstname, middlename, surname, birthday FROM users WHERE id = {msg.author.id}').fetchone()
+            usrdt = dict(zip(info, data))
+            if usrdt['firstname'] == None:
+                back = 'firstname'
+                answer = 'Теперь отчество'
+            elif usrdt['middlename'] == None:
+                back = 'middlename'
+                answer = 'Фамилию...'
+            elif usrdt['surname'] == None:
+                back = 'surname'
+                answer = '...и дату рождения в формате ГГГГ-ММ-ДД (Например: 1950-10-29)'
+            elif usrdt['birthday'] == None:
+                back = 'birthday'
+                answer = 'Всё! Вы зарегистрированы! Удачного RP, Товарищ!'
+            else:
+                a = 1
 
     
     await bot.process_commands(msg)
 
-
-        
 
 bot.run(settings['token'])
