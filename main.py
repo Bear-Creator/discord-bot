835b04dae25c5e8296e041495a1fbc299fa5187e
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2022-04-12 16:33:51 +0300

    регистрация

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+import time
 import discord
 from discord.ext import commands
 import sqlite3
-from config import settings, db
+from config import settings
 
 
 
-def registration():
-    da = 1
+def reg_in_sql(msg, sql):
+    curs.execute(f'INSERT INTO users ({sql}) VALUES ("{msg}")')
+    db.commit
+    
+
+    
 
 bot = commands.Bot(command_prefix=settings['prefix'], intents = discord.Intents.all())
 print('Подключение к БД...')
+
+global db
+global curs
 db = sqlite3.connect('server.db')
 curs = db.cursor()
+
 
 @bot.event
 async def on_ready():
@@ -57,10 +66,12 @@ async def reg(ctx):
         member: discord.PermissionOverwrite(read_messages=True),
     }
     channel = await guild.create_text_channel(f'регистрация {member}', overwrites = overwrites)
+    print(channel)
+    send = bot.get_channel(channel.id)
     await ctx.reply(f'Переходи в эту комнату: <#{channel.id}>')
-    await bot.get_channel(channel.id).send('Приступим!')
-    await bot.get_channel(channel.id).send('Придумай легенду своего пресонажа')
-    await bot.get_channel(channel.id).send('Напишите имя персонажа')
+    await send.send('Приступим!')
+    await send.send('Придумай легенду своего пресонажа')
+    await send.send('Напишите имя персонажа')
     
 
 @bot.command() #Delete user info
@@ -79,26 +90,48 @@ async def on_message(msg):
     if author != bot.user:
         text = msg.content
         print(f'{author}: {text}')
-        if 'регистрация' in channel:
+        if 'регистрация' in str(channel):
+            send_to = bot.get_channel(channel.id)
+
             info = ('firstname', 'middlename', 'surname', 'birthday')
             data = curs.execute(f'SELECT firstname, middlename, surname, birthday FROM users WHERE id = {msg.author.id}').fetchone()
             usrdt = dict(zip(info, data))
+
             if usrdt['firstname'] == None:
                 back = 'firstname'
-                answer = 'Теперь отчество'
+                await send_to.send('Хорошо, теперь отчество')
+
             elif usrdt['middlename'] == None:
                 back = 'middlename'
-                answer = 'Фамилию...'
+                await send_to.send('Фамилию...')
+
             elif usrdt['surname'] == None:
                 back = 'surname'
-                answer = '...и дату рождения в формате ГГГГ-ММ-ДД (Например: 1950-10-29)'
+                await send_to.send('...и дату рождения в формате ГГГГ-ММ-ДД (Например: 1950-10-29)')
+
             elif usrdt['birthday'] == None:
                 back = 'birthday'
-                answer = 'Всё! Вы зарегистрированы! Удачного RP, Товарищ!'
-            else:
-                a = 1
+                await send_to.send('Всё! Вы зарегистрированы! Удачного RP, Товарищ!')
+                time.sleep(5)
 
-    
+                if channel:
+                    await channel.delete()
+
+            else:
+                back = False
+                await send_to.send("Вы уже зарегистрированы!")
+                time.sleep(5)
+
+                if channel:
+                    await channel.delete()
+            
+            if back:
+                print(f'Ввожу {back}')
+                curs.execute(f'UPDATE users SET {back} = "{text}" WHERE id = {author.id}')
+                db.commit()
+
+            
+
     await bot.process_commands(msg)
 
 
